8f292b4b4d25572e7bff10c4778638c1ecdfba30
Thy Dai <kathydai@email.uark.edu>
2017-01-19 16:29:38 -0600

    Added a menubar with 1 menu: File

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,19 +2,18 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
 
 
 public class Launcher extends JPanel implements ActionListener{
@@ -23,82 +22,10 @@ public class Launcher extends JPanel implements ActionListener{
 
 	private JFrame frame;
 	
+	// Menu Bar
 	JMenuBar menuBar;
 	JMenu menu, submenu;
 	JMenuItem menuItem;
-	JRadioButtonMenuItem rbMenuItem;
-	
-	menuBar = new JMenuBar();
-	
-	//Build the first menu.
-	menu = new JMenu("A Menu");
-	menu.setMnemonic(KeyEvent.VK_A);
-	menu.getAccessibleContext().setAccessibleDescription(
-	        "The only menu in this program that has menu items");
-	menuBar.add(menu);
-
-	//a group of JMenuItems
-	menuItem = new JMenuItem("A text-only menu item",
-	                         KeyEvent.VK_T);
-	menuItem.setAccelerator(KeyStroke.getKeyStroke(
-	        KeyEvent.VK_1, ActionEvent.ALT_MASK));
-	menuItem.getAccessibleContext().setAccessibleDescription(
-	        "This doesn't really do anything");
-	menu.add(menuItem);
-
-	menuItem = new JMenuItem("Both text and icon",
-	                         new ImageIcon("images/middle.gif"));
-	menuItem.setMnemonic(KeyEvent.VK_B);
-	menu.add(menuItem);
-
-	menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
-	menuItem.setMnemonic(KeyEvent.VK_D);
-	menu.add(menuItem);
-
-	//a group of radio button menu items
-	menu.addSeparator();
-	ButtonGroup group = new ButtonGroup();
-	rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-	rbMenuItem.setSelected(true);
-	rbMenuItem.setMnemonic(KeyEvent.VK_R);
-	group.add(rbMenuItem);
-	menu.add(rbMenuItem);
-
-	rbMenuItem = new JRadioButtonMenuItem("Another one");
-	rbMenuItem.setMnemonic(KeyEvent.VK_O);
-	group.add(rbMenuItem);
-	menu.add(rbMenuItem);
-
-	//a group of check box menu items
-	menu.addSeparator();
-	cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-	cbMenuItem.setMnemonic(KeyEvent.VK_C);
-	menu.add(cbMenuItem);
-
-	cbMenuItem = new JCheckBoxMenuItem("Another one");
-	cbMenuItem.setMnemonic(KeyEvent.VK_H);
-	menu.add(cbMenuItem);
-
-	//a submenu
-	menu.addSeparator();
-	submenu = new JMenu("A submenu");
-	submenu.setMnemonic(KeyEvent.VK_S);
-
-	menuItem = new JMenuItem("An item in the submenu");
-	menuItem.setAccelerator(KeyStroke.getKeyStroke(
-	        KeyEvent.VK_2, ActionEvent.ALT_MASK));
-	submenu.add(menuItem);
-
-	menuItem = new JMenuItem("Another item");
-	submenu.add(menuItem);
-	menu.add(submenu);
-
-	//Build second menu in the menu bar.
-	menu = new JMenu("Another Menu");
-	menu.setMnemonic(KeyEvent.VK_N);
-	menu.getAccessibleContext().setAccessibleDescription(
-	        "This menu does nothing");
-	menuBar.add(menu);
 	
 	// Left Panel
 	JPanel leftPanel;
@@ -112,6 +39,14 @@ public class Launcher extends JPanel implements ActionListener{
 	
 	public Launcher() {
         frame = new JFrame();
+        
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        menuItem = new JMenuItem("Exit");
+        menu.add(menuItem);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+        
 		tabbedPanels = initGUI();
         
         frame.setTitle("Display Values");
@@ -121,6 +56,8 @@ public class Launcher extends JPanel implements ActionListener{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(tabbedPanels);
 		frame.setVisible(true);
+		
+		
 	}
 		
 	public JTabbedPane initGUI() {		// Building the GUI
@@ -149,7 +86,7 @@ public class Launcher extends JPanel implements ActionListener{
 	} 
 	
 	public JPanel initMainPanel(JPanel mainPanel) {			// Builds the main Panel
-	
+		
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 				
 		leftPanel = new JPanel(new GridLayout(1,1));
