b82e9af384e414bc13d6ade5d698695cb43a0c18
Brett <badixon@uark.edu>
2017-02-06 18:05:14 -0600

    Initial patient file creation and stoplight brain images
    Creates timestamped, plain-text files on Start Record and aphends
    stopping time on Stop Record to end of file.
    Patient files stored in new folder 'patients' for the time being
    New Image folder added
    Master green, yellow, and red images added
    Segmented green, yellow, and red images added

--- src/InfoBars/MenuBar.java
+++ src/InfoBars/MenuBar.java
@@ -6,6 +6,11 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class MenuBar extends JMenuBar{
 	/**
@@ -16,6 +21,10 @@ public class MenuBar extends JMenuBar{
 	JMenu mFile, mView, mActions;
 	JMenuItem miSave, miExit;
 	JMenuItem miStart, miStop;
+	
+	private String fileName = new String();
+	BufferedWriter writer;
+
 	
 	public MenuBar(JFrame frame) {
 		menuBar = new JMenuBar();
@@ -29,10 +38,58 @@ public class MenuBar extends JMenuBar{
 		miStart = new JMenuItem("Start Recording");
 		miStop = new JMenuItem("Stop Recording");
 		
+
+		
+		try{
+		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("fart.txt"), "utf-8"));
+		}
+		catch (Exception e) {System.out.println("Terrible error");}
+		
 		miExit.addActionListener((ActionEvent event) -> {
 			System.exit(0);
 		});
 		
+		miStart.addActionListener((ActionEvent event) -> {
+			String fName = new String();
+			String lName = new String();
+			String ssn = new String();
+			// The following should probably become a single prompt in the future
+			fName = JOptionPane.showInputDialog(this, "First Name:");
+			lName = JOptionPane.showInputDialog(this, "Last Name:");
+			ssn = JOptionPane.showInputDialog(this, "Social Security Number:");
+			
+			String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+			String fileTime = new SimpleDateFormat("MMM dd yyyy 'at' HH:mm:ss").format(new Date());
+			fileName = "./src/patients/" + fName + "_" + lName + "_" + timeStamp + ".txt";
+			
+			try {
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "utf-8"));
+				writer.write("Patient: " + fName + " " + lName);
+				writer.newLine();
+				writer.write("SSN: " + ssn);
+				writer.newLine();
+				writer.write("Start Recording: " + fileTime);
+				writer.newLine();
+				writer.write("--------------------------------------------------------------------------------------------");
+				writer.newLine();
+				writer.close();
+			}
+			catch (Exception e) {}
+		});
+		
+		miStop.addActionListener((ActionEvent event) -> {
+			String fileTime = new SimpleDateFormat("MMM dd yyyy 'at' HH:mm:ss").format(new Date());
+			try{
+				FileWriter fstream = new FileWriter(fileName, true);
+				writer = new BufferedWriter(fstream);
+				writer.write("--------------------------------------------------------------------------------------------");
+				writer.newLine();
+				writer.write("End Recording: " + fileTime);
+				writer.close();
+			}
+			catch (Exception e) {}
+		});
+				
 		mFile.add(miSave);
 		mFile.add(miExit);
 		
